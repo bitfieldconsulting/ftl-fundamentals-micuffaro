573b4b48a280027cef81712b888f3f2a0e919fcb
Michael Cuffaro <michael@cuffaro.com>
2021-01-13 18:12:31 +0100

    feat: exercise 09

---
+++ 09/calculator.go
@@ -0,0 +1,16 @@
+package calculator
+
+// Subtract subtracts two integers
+func Subtract(a, b int) int {
+	return a - b
+}
+
+// Multiply multiplies two integers
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide adds two integers
+func Divide(a, b int) int {
+	return a / b
+}
